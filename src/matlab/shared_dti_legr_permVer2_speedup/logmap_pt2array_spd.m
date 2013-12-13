cac9a4c5846fd5ccfb692ff676222800049514ac
Hyunwoo Kim <kimbellw@gmail.com>
2013-12-13 15:03:09 -0600

    Let's remove redundant calculation

---
+++ src/matlab/shared_dti_legr_permVer2_speedup/logmap_pt2array_spd.m
@@ -0,0 +1,21 @@
+function V = logmap_pt2array_spd(p,X)
+%LOGMAP_PT2ARRAY_SPD maps X from GL(n)/O(n) to TpM at xi.
+
+
+
+[U D] = eig(p);
+g = U*sqrt(D);
+invg = inv(g);
+
+
+%% For each data
+for
+    if norm(P-X) < 1e-18
+        v = zeros(size(P));
+        return 
+    end
+    y = invg*X*invg';
+    [V S] = eig(y);
+    H = g*V;
+    v = H*diag(log(diag(S)))*H';
+end
