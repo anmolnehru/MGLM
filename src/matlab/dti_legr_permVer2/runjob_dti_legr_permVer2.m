7d58524624e01b3bc93466ad8854450107da92cf
Hyunwoo Kim <kimbellw@gmail.com>
2013-12-13 19:52:49 -0600

    Check speed up code the error calculation part

--- src/matlab/dti_legr_permVer2/runjob_dti_legr_permVer2.m
+++ src/matlab/dti_legr_permVer2/runjob_dti_legr_permVer2.m
@@ -12,10 +12,10 @@ load('idx_dti_test_100.mat');
 
 mxstack = mxstack_job;
 disp('Data Loaded.')
-t1 = clock;
+
 nvoxels = size(mask_job,1);
 ErrMx = zeros(nvoxels,size(idx_dti,1));
-
+t1 = clock;
 for imask = 1:nvoxels
 %    tic
 %    fprintf('%d/%d\n ',imask,nvoxels);
@@ -31,6 +31,6 @@ for imask = 1:nvoxels
     end
 %    toc;
 end
-save(strcat([exp_name,'_result_legr_dti_Ver2']),'ErrMx','mask_job');
 t2 = clock;
+save(strcat([exp_name,'_result_legr_dti_Ver2']),'ErrMx','mask_job');
 elapsedtime = etime(t2,t1)
--- src/matlab/dti_legr_permVer2_speedup/runjob_dti_legr_permVer3_speedup.m
+++ src/matlab/dti_legr_permVer2_speedup/runjob_dti_legr_permVer3_speedup.m
@@ -1,23 +1,23 @@
 % runjob_dti_legr
 % shared job code
 %clear;
-load('data');
+%load('data');
 %load('idx_dti_test.mat');
-load('idx_dti_test_100.mat');
+%load('idx_dti_test_100.mat');
 % exp_name
 % X
 % mxstack
 % mask
 
 disp('Data Loaded.')
-t1 = clock;
+
 nvoxels = size(mask_job,1);
 ErrMx = zeros(nvoxels,size(idx_dti,1));
-
+t1 = clock;
 for imask = 1:nvoxels
     ErrMx(imask,:) = GR_legr_spd_perm(Xs, mxstack_job{imask},idx_dti);
 end
-
-save(strcat([exp_name,'_result_legr_dti_Ver2']),'ErrMx','mask_job');
 t2 = clock;
+%save(strcat([exp_name,'_result_legr_dti_Ver2']),'ErrMx','mask_job');
+
 elapsedtime = etime(t2,t1)
