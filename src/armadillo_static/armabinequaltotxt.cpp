e44b363543df943ab6fe28ec45d54da5775ea47f
mlman <kimbellw@gmail.com>
2015-08-10 20:09:22 -0500

    arma save and load examples.

--- src/armadillo_static/armabinequaltotxt.cpp
+++ src/armadillo_static/armabinequaltotxt.cpp
@@ -19,30 +19,43 @@
 
 #include "spd_funcs.h"
 #include "gr_spd.h"
-
+#define SZBUF 300
 using namespace std;
 using namespace arma;
 namespace fs=boost::filesystem;
 
 int main(int argc, char** argv)
 {
-  int seed = time(NULL);
-  srand(seed);
-  printf("Hello World.\n");
-  mat A = randu<mat>(10,5);
-  A.print();
-  A.save("A.bin", arma_binary);
-  mat B;
-  B.load("A.bin",arma_binary);
-  mat C;
-  C = A-B;
-  double error = sum(sum(abs(C)));
-  if(error == 0.0){
-    printf("No error.\n");
-  }
-  else{
-    printf("Error is %lf.\n",error);
-  }
+    int seed = time(NULL);
+    srand(seed);
+	// Let's consider binary file read and write later
+    if(argc < 3){
+        printf("Usage: >> armabinequaltotxt filename.bin filename.txt");
+        return 0;
+    }
+    char fbinname[SZBUF+1];
+    char ftxtname[SZBUF+1];
+    if(argc >= 3){
+        strncpy(fbinname, argv[1], SZBUF);
+    }
+    if(argc >= 3){
+        strncpy(ftxtname, argv[2], SZBUF);
+    }
 
-  return 0;
+    mat A, B;
+    A.load(fbinname,arma_binary);
+    B.load(ftxtname,raw_ascii);
+    mat C;
+    C = A-B;
+    double error = sum(sum(abs(C)));
+    if(error == 0.0){
+      printf("No error.\n");
+    }
+    else{
+      printf("Error is %lf.\n",error);
+      A.print("A:");
+      B.print("B:");
+    }
+
+    return 0;
 }
