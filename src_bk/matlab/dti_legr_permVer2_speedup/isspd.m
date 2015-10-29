2ff7a0da7a52e313dee15624c91218f0e987a5a3
Anmol Mohanty <anmolnehru@gmail.com>
2015-10-29 14:09:45 -0500

    2pac

--- src_bk/matlab/dti_legr_permVer2_speedup/isspd.m
+++
@@ -1,6 +0,0 @@
-function T = isspd(mx)
-% Check matrices are symmetric positive definite.
-T = zeros(size(mx,3),1);
-for i=1:size(mx,3)
-   T(i) = (sum(eig(mx(:,:,i)) <= 0 ) ==0);
-end
--- src_bk/matlab/odf_legr_permVer2/prediction.m
+++
@@ -1,8 +0,0 @@
-function phat = prediction(p,V,X,expmap)
-%prediction predicts values based on one or multiple tangent vectors
-ndata = length(X);
-phat = zeros(size(p,1),ndata);
-
-for i = 1:ndata
-    phat(:,i) = expmap(p,V*X(:,i));
-end
