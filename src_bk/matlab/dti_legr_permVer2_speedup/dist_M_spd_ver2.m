e734435b49f46acfcf6a3e6eceb3d1988c3a00cd
Anmol Mohanty <anmolnehru@gmail.com>
2015-10-29 14:09:45 -0500

    2pac

--- src_bk/matlab/dti_legr_permVer2_speedup/dist_M_spd_ver2.m
+++
@@ -1,8 +0,0 @@
-function d = dist_M_spd_ver2(X,Y)  
-    [U D ] = eig(X);
-    invg = U*diag(1./sqrt(diag(D)))*U';
-    [U D ] = eig(invg*Y*invg);
-    d = sqrt(sum(diag(log(D)).^2));
-    
-    
-end
--- src_bk/matlab/dti_legr_permVer2_speedup/save_uint8.m
+++
@@ -1,9 +0,0 @@
-function save_uint8(fname,mx)
-fid = fopen(fname,'w');
-
-for irow = 1:size(mx,1)
-    fprintf(fid, '%d ', mx(irow,:));
-    fprintf(fid,'\n');
-end
-
-fclose(fid);
